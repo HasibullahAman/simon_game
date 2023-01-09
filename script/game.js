287645b462ad94ed562862b4fb0ede0d25e6985f
Hasibullah Aman <hasib.aman7@gmail.com>
2023-01-09 06:43:54 +0430

    03

--- script/game.js
+++ script/game.js
@@ -2,7 +2,6 @@ var gamePattern = new Array();
 var buttonColor = ['red', 'green', 'blue','yellow'];
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4)
-    // console.log(randomNumber);
     return randomNumber;
 }
 
@@ -10,4 +9,4 @@ var randomChosenColour = buttonColor[nextSequence()];
 console.log(randomChosenColour);
 gamePattern.push(randomChosenColour);
 
-$(randomChosenColour).fadeOut(1000).fadeIn(1000).fadeOut(1000).fadeIn(1000);
+$('#randomChosenColour').fadeOut(1000).fadeIn(1000).fadeOut(1000).fadeIn(1000);
